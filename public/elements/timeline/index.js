69221b4d8a02b18cc68a094474baca8ee1f325d0
Joe Fleming <joe.fleming@gmail.com>
2017-09-29 15:49:24 -0700

    feat: add a timeline element

---
+++ public/elements/timeline/index.js
@@ -0,0 +1,23 @@
+import vis from 'vis';
+import 'vis/dist/vis.css';
+import { Element } from 'plugins/canvas/elements/element';
+import { header } from './header.png';
+
+export default new Element('timeline', {
+  displayName: 'Timeline',
+  description: 'A timeline vis, for showing events over a period of time',
+  image: header,
+  expression: 'filters | demodata | pointseries x="project" y="state" size="median(price)" | grid | render',
+  render(domNode, config, handlers) {
+    const { options, data, font } = config;
+    const items = data
+    .filter(row => Boolean(row.content) && Boolean(row.start))
+    .map(row => ({
+      ...row,
+      start: row.start && new Date(row.start),
+      end: row.end && new Date(row.end),
+    }));
+
+    new vis.Timeline(domNode, items, options);
+  },
+});
--- public/lib/load_plugin.js
+++ public/lib/load_plugin.js
@@ -1,4 +1,7 @@
 import { functions as functionsRegistry } from 'plugins/canvas/lib/functions';
+import { elements as elementsRegistry } from 'plugins/canvas/lib/elements';
 import { commonFunctions } from '../../common/functions';
+import { elements } from '../elements';
 
 commonFunctions.forEach(fnDef => functionsRegistry.register(fnDef));
+elements.forEach(elDef => elementsRegistry.register(elDef));
