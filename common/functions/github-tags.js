54222df8747fb4010195b0876cb29e5c5b8afabe
Joe Fleming <joe.fleming@gmail.com>
2017-09-29 12:37:47 -0700

    add support for oauth tokens

--- common/functions/github-tags.js
+++ common/functions/github-tags.js
@@ -2,18 +2,19 @@ import axios from 'axios';
 import mem from 'mem';
 import Fn from '../../../kibana-canvas/common/functions/fn.js'; // TODO: internalize this wrapper
 
-const getTagSha = (tag) => {
-  return axios.get(tag.commit.url)
+const getTagSha = (tag, headers) => {
+  return axios.get(tag.commit.url, { headers })
   .then(res => res.data)
   .then(commit => ({
     name: tag.name,
     sha: commit.sha,
+    committer: commit.commit.committer.name,
     date: commit.commit.committer.date,
   }));
 };
 
 const getTagShaMemoized = mem(getTagSha, {
-  cacheKey: tag => tag.commit.sha
+  cacheKey: tag => tag.commit.sha,
 });
 
 export default new Fn({
@@ -28,15 +29,18 @@ export default new Fn({
     limit: {
       types: ['number', null],
       default: 10,
-    }
+    },
+    token: {
+      types: ['string', null],
+    },
   },
-  fn(context, args) {
-    const tagsUrl = `https://api.github.com/repos/${args.repo}/tags`;
-    const tagLimit = args.limit;
+  fn(context, { repo, token, limit: tagLimit }) {
+    const tagsUrl = `https://api.github.com/repos/${repo}/tags`;
+    const headers = token ? { Authorization: `token ${token}` } : {};
 
-    return axios.get(tagsUrl)
+    return axios.get(tagsUrl, { headers })
     .then(res => res.data)
-    .then(tags => Promise.all(tags.slice(0, tagLimit).map(getTagShaMemoized)))
+    .then(tags => Promise.all(tags.slice(0, tagLimit).map(tag => getTagShaMemoized(tag, headers))))
     .then((rows) => ({
       type: 'datatable',
       columns: Object.keys(rows[0]),
